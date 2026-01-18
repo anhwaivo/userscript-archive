8286ab4bc6cc486e1148bf5ead6a37a22f3dc79e
Anhwaivo <97143764+anhwaivo@users.noreply.github.com>
2026-01-18 17:47:15 +0700

    create userscri

---
+++ adunezz.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Edunext Skibidi1
+// @namespace    https://bio.anhwaivo.xyz
+// @version      1.5
+// @description  edunext skibidi - Alphatest v1.5
+// @author       Anhwaivo
+// @match        https://fsc-edunext.fpt.edu.vn/*
+// @match        https://chatbot.fpt.edu.vn/*
+// @grant        unsafeWindow
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_xmlhttpRequest
+// @require      https://github.com/AugmentedWeb/UserGui/raw/Release-1.0/usergui.js
+// @require      https://raw.githubusercontent.com/anhwaivo/userscript-archive/refs/heads/main/adunezz.js
+// @run-at       document-start
+// ==/UserScript==
